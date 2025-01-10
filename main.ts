f94b831c1d65efa3ce5b732e0e54e513e66b9d0e
Axel Huerta <axelhuerta.dev@gmail.com>
2025-01-09 19:02:25 -0600

    add sofipos

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import { getTasas } from "./controllers/tasas.controller.ts";
 import { getUdis } from "./controllers/udis.controller.ts";
+import { getSofipos } from "./controllers/sofipos.controller.ts";
 
 const app = new Hono();
 
@@ -16,6 +17,11 @@ app.get("/tasas", async (c) => {
 app.get("/udis", async (c) => {
   const udis = await getUdis();
   return c.json(udis);
+});
+
+app.get("/sofipos", async (c) => {
+  const sofipos = await getSofipos();
+  return c.json(sofipos);
 });
 
 Deno.serve(app.fetch);
