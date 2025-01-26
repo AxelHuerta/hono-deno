88acb7f9fdf8c0503498dc5f39adea49b8832422
Axel Huerta <axelhuerta.dev@gmail.com>
2025-01-26 17:49:02 -0600

    add some sofipos

--- services/sofipos.service.ts
+++ services/sofipos.service.ts
@@ -41,9 +41,32 @@ export async function getSofiposService() {
     difference: 0,
   };
 
+  // https://sofipos.mx/admin/nicap/pdfs/23_4v7ln.pdf
+  const stori: Sofipo = {
+    name: "stori",
+    year: 2024,
+    category: 1,
+    month: 11,
+    value: 148.7868,
+    previewValue: 197.2791,
+    difference: 0,
+  };
+
+  const supertasas: Sofipo = {
+    name: "supertasas",
+    year: 2024,
+    category: 1,
+    month: 11,
+    value: 517.4,
+    previewValue: 161.84,
+    difference: 0,
+  };
+
   klar.difference = klar.value - klar.previewValue;
   nu.difference = nu.value - nu.previewValue;
   finsus.difference = finsus.value - finsus.previewValue;
+  stori.difference = stori.value - stori.previewValue;
+  supertasas.difference = supertasas.value - supertasas.previewValue;
 
-  return [klar, nu, finsus];
+  return [klar, nu, finsus, stori, supertasas];
 }
